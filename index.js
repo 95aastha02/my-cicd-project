ecc68087b0beb25686e3a04785f8496e97f2b080
aastha <adhikariaastha321@gmail.com>
2026-05-23 17:15:42 +0545

    initialial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8001;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Hello, I am Aastha</h1>
+    <h2>Full CI/CD Pipeline Assignment</h2>
+    <p>This project is deployed automatically using GitHub Actions.</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
